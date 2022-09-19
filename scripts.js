12a62cc4c6fe479e0cf52cd28e29c48c9cc07f8b
Douglas K Lopes <douglasklopes@hotmail.com>
2022-09-19 17:43:57 -0300

    add function add games and channels

--- scripts.js
+++ scripts.js
@@ -1,15 +1,65 @@
-var modal = document.getElementById("modal-games");
+// Games Functions
+var modalGames = document.getElementById("modal-games");
+var gamesList = document.getElementsByClassName("games-list")[0];
 
-var btn = document.getElementById("btn-add-games");
-
-var span = document.getElementsByClassName("btn-close")[0];
-
-btn.onclick = () => {
-    modal.style.display = "block";
+function addGameList(){
+    modalGames.style.display = "block";
 }
 
-span.onclick = () => {
-    modal.style.display = "none"
-} 
+function closeModalGames() {
+    modalGames.style.display= "none";
+}
+
+function addGame() {
+    let imgGame = document.querySelector("#img-game");
+    let linkGame = document.querySelector("#link-game");
+
+    gamesList.innerHTML = `
+    <li>
+        <a 
+        target="_blank"
+        href="${linkGame.value}">
+            <img src="${imgGame.value}" alt="" />
+        </a>
+    </li>` + gamesList.innerHTML;
+ 
+
+     alert(`Adicionado com sucesso!`);
+     modalGames.style.display = "none";
+}
+
+// Cannel Functions
+var modalChannel = document.getElementById("modal-channel");
+var channelList = document.getElementsByClassName("channel-list")[0];
+
+function addChannelList(){
+    modalChannel.style.display = "block";
+}
+
+function closeModalChannel() {
+    modalChannel.style.display= "none";
+}
+
+function addChannel() {
+    let imgChannel = document.querySelector("#img-channel");
+    let linkChannel = document.querySelector("#link-channel");
+
+    channelList.innerHTML = `
+    <li>
+        <a 
+        target="_blank"
+        href="${linkChannel.value}">
+            <img src="${imgChannel.value}" alt="" />
+        </a>
+    </li>` + channelList.innerHTML;
+ 
+
+     alert(`Adicionado com sucesso!`);
+     modalChannel.style.display = "none";
+}
+
+
+
+
 
 
